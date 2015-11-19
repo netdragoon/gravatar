ce71a2573cfc234f81f01435006f488f6c949930
netdragoon <netdragoon@outlook.com>
2015-11-19 21:30:54 -0200

    update

--- Canducci.Gravatar.Console4.5.1/Program.cs
+++ Canducci.Gravatar.Console4.5.1/Program.cs
@@ -11,8 +11,8 @@ namespace Canducci.Gravatar.Console4._5._1
         static void Main(string[] args)
         {
             string folder = "image/";
-            string filename = "foto250a";
-            int width = 250;
+            string filename = "foto300a";
+            int width = 300;
 
             IEmail email = new Email("fulviocanducci@hotmail.com");
 
@@ -23,16 +23,16 @@ namespace Canducci.Gravatar.Console4._5._1
 
             if (Avatar.Exists(configuration, folder, filename, out path))
             {
-                System.Console.WriteLine("Foto existente: {0}", path);
+                Console.WriteLine("Foto existente: {0}", path);
             }
             else
             {
                 IAvatar avatar = new Avatar(configuration);
                 avatar.SaveAs(folder, filename);
-                System.Console.WriteLine("Foto gravada com sucesso !!!");
+                Console.WriteLine("Foto gravada com sucesso !!!");
             }
 
-            System.Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
