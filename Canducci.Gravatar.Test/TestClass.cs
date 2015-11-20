16f9c9f80491c718b7e384bea5d7d03e165a2e6f
netdragoon <netdragoon@outlook.com>
2015-11-20 10:50:09 -0200

    Actual

---
+++ Canducci.Gravatar.Test/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+namespace Canducci.Gravatar.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]        
+        public void EmailFailedTest()
+        {
+            Assert.Throws(typeof(InvalidOperationException),
+                new TestDelegate(EmailFailed));                       
+        }
+        private void EmailFailed()
+        {
+            IEmail email = new Email("");            
+        }
+
+        [TestCase]
+        public void EmailOkTest()
+        {
+            IEmail email = new Email("test@test.com");
+            Assert.IsInstanceOf<Email>(email);
+        }
+    }
+}
